c71bc2c78a12e578edc4ac1e742ff16595ec6c92
Linda Perez <rosmery.p.p@gmail.com>
2019-04-02 18:34:32 -0700

    First Week React Components Part 2 Select a dish from Menu and display content

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import Menu  from './components/MenuComponent';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
+
+
 
 
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+
+    }
+
   render() {
     return (
       <div >
@@ -14,7 +26,7 @@ class App extends Component {
           </NavbarBrand>
         </div>
         </Navbar>
-          <Menu />
+          <Menu dishes = { this.state.dishes}/>
       </div>
     );
   }
